2be07fc356db346a41bfbab07a71c23c761fc2e6
James Robert <jiaaro@gmail.com>
2011-05-02 15:48:28 -0400

    added package info for installing

---
+++ setup.py
@@ -0,0 +1,34 @@
+__doc__ = """
+Manipulate audio with an simple and easy high level interface.
+
+See the README file for details, usage info, and a list of gotchas.
+"""
+
+from setuptools import setup
+
+setup(
+    name='pydub',
+    version='0.1',
+    author='James Robert',
+    author_email='jiaaro@gmail.com',
+    description='Manipulate audio with an simple and easy high level interface',
+    license='MIT',
+    keywords='audio sound high-level',
+    url='http://pydub.com',
+    packages=['pydub'],
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Topic :: Multimedia :: Sound/Audio :: Conversion",
+        "Topic :: Multimedia :: Sound/Audio :: Editors",
+        "Topic :: Multimedia :: Sound/Audio :: Mixers",
+        "Topic :: Software Development :: Libraries",
+        'Topic :: Utilities',
+    ]
+)
