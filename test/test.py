c483e390e0ecd20eff1e9cd477885420c6b884f1
James Robert <jiaaro@gmail.com>
2012-05-30 11:42:40 -0400

    update and add tests for new features

--- test/test.py
+++ test/test.py
@@ -75,10 +75,10 @@ class AudioSegmentTests(unittest.TestCase):
 
     def test_volume_with_add_sub(self):
         quieter = self.seg1 - 6
-        self.assertAlmostEqual(ratio_to_db(quieter.rms, self.seg1.rms), -6)
+        self.assertAlmostEqual(ratio_to_db(quieter.rms, self.seg1.rms), -6, places=2)
 
         louder = quieter + 2.5
-        self.assertAlmostEqual(ratio_to_db(louder.rms, quieter.rms), 2.5)
+        self.assertAlmostEqual(ratio_to_db(louder.rms, quieter.rms), 2.5, places=2)
 
     def test_repeat_with_multiply(self):
         seg = self.seg1 * 3
@@ -161,14 +161,16 @@ class AudioSegmentTests(unittest.TestCase):
 
     def test_fades(self):
         seg = self.seg1[:10000]
+        
         # 1 ms difference in the position of the end of the fade out
         inf_end = seg.fade(start=0, end=float('inf'), to_gain=-120)
         negative_end = seg.fade(start=0, end=-1, to_gain=-120)
 
         self.assertWithinTolerance(inf_end.rms, negative_end.rms,
                 percentage=0.001)
-        self.assertTrue(negative_end.rms < inf_end.rms)
+        self.assertTrue(negative_end.rms <= inf_end.rms)
         self.assertTrue(inf_end.rms < seg.rms)
+        
         self.assertEqual(len(inf_end), len(seg))
 
         self.assertTrue(-3 < ratio_to_db(inf_end.rms, seg.rms) < -2)
@@ -191,7 +193,27 @@ class AudioSegmentTests(unittest.TestCase):
         db_at_beginning = ratio_to_db(fade_out[:1000].rms, seg[:1000].rms)
         db_at_end = ratio_to_db(fade_out[-1000:].rms, seg[-1000:].rms)
         self.assertTrue(db_at_end < db_at_beginning)
-
+        
+    def test_reverse(self):
+        seg = self.seg1
+        rseg = seg.reverse()
+        
+        # the reversed audio should be exactly equal in playback duration
+        self.assertEqual(len(seg), len(rseg))
+        
+        r2seg = rseg.reverse()
+        
+        # if you reverse it twice you should get an identical AudioSegment
+        self.assertEqual(seg, r2seg)
+        
+    def test_normalize(self):
+        seg = self.seg1
+        normalized = seg.normalize(0.0)
+        
+        self.assertEqual(len(normalized), len(seg))
+        self.assertGreater(normalized.rms, seg.rms)
+        self.assertWithinTolerance(normalized.max, normalized.max_possible_amplitude, percentage=0.0001)
+        
     def test_for_accidental_shortening(self):
         seg = AudioSegment.from_mp3(os.path.join(data_dir, 'party.mp3'))
         seg.export('tmp.mp3')
