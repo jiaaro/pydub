ba6d17871a545e9ee17da50dde2a64ea8dea8540
James Robert <github@jiaaro.com>
2014-10-22 15:07:02 -0400

    Update audio_segment.py

--- pydub/audio_segment.py
+++ pydub/audio_segment.py
@@ -30,6 +30,37 @@ if sys.version_info >= (3, 0):
     basestring = str
     xrange = range
     StringIO = BytesIO
+    
+
+class ClassPropertyDescriptor(object):
+
+    def __init__(self, fget, fset=None):
+        self.fget = fget
+        self.fset = fset
+
+    def __get__(self, obj, klass=None):
+        if klass is None:
+            klass = type(obj)
+        return self.fget.__get__(obj, klass)()
+
+    def __set__(self, obj, value):
+        if not self.fset:
+            raise AttributeError("can't set attribute")
+        type_ = type(obj)
+        return self.fset.__get__(obj, type_)(value)
+
+    def setter(self, func):
+        if not isinstance(func, (classmethod, staticmethod)):
+            func = classmethod(func)
+        self.fset = func
+        return self    
+
+def classproperty(func):
+    if not isinstance(func, (classmethod, staticmethod)):
+        func = classmethod(func)
+
+    return ClassPropertyDescriptor(func)
+
 
 AUDIO_FILE_EXT_ALIASES = {
     "m4a": "mp4",
@@ -52,8 +83,13 @@ class AudioSegment(object):
 
     # TODO: remove in 1.0 release
     # maintain backwards compatibility for ffmpeg attr (now called converter)
-    ffmpeg = property(lambda s: s.converter,
-                      lambda s, v: setattr(s, 'converter', v))
+    @classproperty
+    def ffmpeg(cls):
+        return cls.converter
+    
+    @ffmpeg.setter
+    def ffmpeg(cls, val):
+        cls.converter = val
 
     DEFAULT_CODECS = {
         "ogg": "libvorbis"
