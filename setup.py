d7b0eb8ce53584d4b1c38698debbbd375af72d2e
James Robert <github@jiaaro.com>
2014-10-24 09:12:04 -0400

    new release for fix in ffmpeg assignment shim

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pydub',
-    version='0.9.2',
+    version='0.9.3',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description='Manipulate audio with an simple and easy high level interface',
