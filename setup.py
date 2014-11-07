f600735ba757fae7b2d5aded36b273f4eb81b814
James Robert <me@jiaaro.com>
2014-11-07 11:06:25 -0500

    increment version for bug fixes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pydub',
-    version='0.9.3',
+    version='0.9.4',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description='Manipulate audio with an simple and easy high level interface',
