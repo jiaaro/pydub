65877e88ab9819ed0a505d3cedd2b180bf6f30fa
James Robert <github@jiaaro.com>
2014-11-13 09:39:30 -0500

    Update playback.py

--- pydub/playback.py
+++ pydub/playback.py
@@ -1,3 +1,9 @@
+"""
+Support for playing AudioSegments - pyaudio is a *much* nicer solution, but
+is tricky to install. See my notes on installing pyaudio in a virtualenv (on 
+OSX 10.10): https://gist.github.com/jiaaro/9767512210a1d80a8a0d
+"""
+
 import subprocess
 from tempfile import NamedTemporaryFile
 from .utils import get_player_name, make_chunks
