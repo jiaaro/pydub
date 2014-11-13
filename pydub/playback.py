434695611c72ba820ede43f2aa61e693a76075de
James Robert <github@jiaaro.com>
2014-11-13 09:40:18 -0500

    Update playback.py

--- pydub/playback.py
+++ pydub/playback.py
@@ -1,5 +1,6 @@
 """
-Support for playing AudioSegments - pyaudio is a *much* nicer solution, but
+Support for playing AudioSegments. Pyaudio will be used if it's installed,
+otherwise will fallback to ffplay. Pyaudio is a *much* nicer solution, but
 is tricky to install. See my notes on installing pyaudio in a virtualenv (on 
 OSX 10.10): https://gist.github.com/jiaaro/9767512210a1d80a8a0d
 """
