0a9c10b22271ebe94c04a6b031cf0040b5d662d0
James Robert <jiaaro@gmail.com>
2014-11-18 11:11:29 -0500

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pydub',
-    version='0.9.4',
+    version='0.9.5',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description='Manipulate audio with an simple and easy high level interface',
