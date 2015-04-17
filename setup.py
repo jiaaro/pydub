c4266e2de376475974dd20c66da747d2e23182b9
James Robert <github@jiaaro.com>
2015-04-17 10:33:55 -0400

    Increment version for new max_dBFS property

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pydub',
-    version='0.10.0',
+    version='0.11.0',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description='Manipulate audio with an simple and easy high level interface',
