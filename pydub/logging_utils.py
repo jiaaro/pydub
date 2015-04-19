86210baf951613eb748f960dbb7471858606e26a
James Robert <jiaaro@gmail.com>
2015-04-19 15:50:43 -0400

    fix ffmpeg debug logging

--- pydub/logging_utils.py
+++ pydub/logging_utils.py
@@ -6,4 +6,4 @@ import logging
 converter_logger = logging.getLogger("pydub.converter")
 
 def log_conversion(conversion_command):
-    converter_logger.debug(u"subprocess.call({0})".format(repr(conversion_command)))
+    converter_logger.debug("subprocess.call(%s)", repr(conversion_command))
