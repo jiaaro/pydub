29c0e2f94b4046bc777e4c2fc6dbc4ac195e6269
James Robert <me@jiaaro.com>
2016-10-12 10:16:03 -0400

    Add clarifying comment to butterworth filter

--- pydub/scipy_effects.py
+++ pydub/scipy_effects.py
@@ -14,12 +14,15 @@ from .utils import register_pydub_effect
 
 def _mk_butter_filter(freq, type, order):
     """
-
     Args:
         freq: The cutoff frequency for highpass and lowpass filters. For
             band filters, a list of [low_cutoff, high_cutoff]
         type: "lowpass", "highpass", or "band"
-        order: nth order butterworth filter (default: 5th order)
+        order: nth order butterworth filter (default: 5th order). The
+            attenuation is -6dB/octave beyond the cutoff frequency (for 1st
+            order). A Higher order filter will have more attenuation, each level
+            adding an additional -6dB (so a 3rd order butterworth filter would
+            be -18dB/octave).
 
     Returns:
         function which can filter a mono audio segment
