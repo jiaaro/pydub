0ea95fa57419b77150bb8e4ba264aa56cf51da86
James Robert <me@jiaaro.com>
2017-05-09 19:01:41 -0400

    Increment version for multichannel splitting

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pydub',
-    version='0.18.0',
+    version='0.19.0',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description='Manipulate audio with an simple and easy high level interface',
