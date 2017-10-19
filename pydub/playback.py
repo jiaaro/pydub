612d9c3d10d89967b74e49ea349d24a84aeaaca5
pmazgaj <przemek.mazgaj@gmail.com>
2017-10-19 22:56:31 +0200

    Extend play function. Enforce PEP-8 peacekeeping forces.

--- pydub/playback.py
+++ pydub/playback.py
@@ -12,14 +12,13 @@ from .utils import get_player_name, make_chunks
 PLAYER = get_player_name()
 
 
-
 def _play_with_ffplay(seg):
     with NamedTemporaryFile("w+b", suffix=".wav") as f:
         seg.export(f.name, "wav")
         subprocess.call([PLAYER, "-nodisp", "-autoexit", "-hide_banner", f.name])
 
 
-def _play_with_pyaudio(seg):
+def _play_with_pyaudio(seg, sound_len=None, chunq_frequency=500):
     import pyaudio
 
     p = pyaudio.PyAudio()
@@ -28,9 +27,13 @@ def _play_with_pyaudio(seg):
                     rate=seg.frame_rate,
                     output=True)
 
+    freq = chunq_frequency
+
     # break audio into half-second chunks (to allows keyboard interrupts)
-    for chunk in make_chunks(seg, 500):
-        stream.write(chunk._data)
+    for chunk in make_chunks(seg, chunq_frequency):
+        if sound_len is not None and freq < sound_len:
+            stream.write(chunk._data)
+            freq += chunq_frequency
 
     stream.stop_stream()
     stream.close()
@@ -38,9 +41,9 @@ def _play_with_pyaudio(seg):
     p.terminate()
 
 
-def play(audio_segment):
+def play(audio_segment, sound_len=None):
     try:
         import pyaudio
-        _play_with_pyaudio(audio_segment)
+        _play_with_pyaudio(audio_segment, sound_len)
     except ImportError:
         _play_with_ffplay(audio_segment)
