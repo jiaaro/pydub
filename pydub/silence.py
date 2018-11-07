6e6f1dc8652476f80ad5e82b63e2581b1510fb62
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2018-11-06 20:16:15 -0800

    Update silence.py
    Found this piece of code at https://stackoverflow.com/questions/29547218/remove-silence-at-the-beginning-and-at-the-end-of-wave-files-with-pydub and tested it offline with delight. I am not the only one who needs this by the looks of https://stackoverflow.com/questions/29547218/remove-silence-at-the-beginning-and-at-the-end-of-wave-files-with-pydub . Hence contributing here.

--- pydub/silence.py
+++ pydub/silence.py
@@ -114,3 +114,20 @@ def split_on_silence(audio_segment, min_silence_len=1000, silence_thresh=-16, ke
         chunks.append(audio_segment[start_i:end_i])
 
     return chunks
+
+
+def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
+    '''
+    sound is a pydub.AudioSegment
+    silence_threshold in dB
+    chunk_size in ms
+    iterate over chunks until you find the first one with sound
+    '''
+    trim_ms = 0 # ms
+    assert chunk_size > 0 # to avoid infinite loop
+    while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+        trim_ms += chunk_size
+
+    return trim_ms
+
+
