cc96c3d93c86d3a11680eed5da833f7a9a9943fc
James Robert <me@jiaaro.com>
2021-03-06 11:08:38 -0600

    v0.25.0
    Signed-off-by: James Robert <me@jiaaro.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pydub',
-    version='0.24.1',
+    version='0.25.0',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description='Manipulate audio with an simple and easy high level interface',
