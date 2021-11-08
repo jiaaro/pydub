cfa7f18b89f9cac6d6cdbd71b7fc883e7bf519a8
Enes Albay <albayenes@gmail.com>
2021-11-09 02:11:35 +0300

    split_on_nonsilence function added. It can be used to get silence part of sound file.

--- pydub/silence.py
+++ pydub/silence.py
@@ -161,6 +161,60 @@ def split_on_silence(audio_segment, min_silence_len=1000, silence_thresh=-16, ke
         audio_segment[ max(start,0) : min(end,len(audio_segment)) ]
         for start,end in output_ranges
     ]
+	
+	
+def split_on_nonsilence(audio_segment, min_silence_len=1000, silence_thresh=-16, keep_voice=100,
+                     seek_step=1):
+    """
+    Returns list of audio segments from splitting audio_segment on nonsilent sections
+
+    audio_segment - original pydub.AudioSegment() object
+
+    min_silence_len - (in ms) minimum length of a silence to be used for
+        a split. default: 1000ms
+
+    silence_thresh - (in dBFS) anything quieter than this will be
+        considered silence. default: -16dBFS
+
+    keep_voice - (in ms or True/False) leave or remove some nonsilence at the beginning
+        and end of the chunks. Keeps the sound from sounding like it
+        is abruptly cut off.
+        When the length of the nonsilence is less than the keep_voice duration
+        it is split evenly between the preceding and following silent
+        segments.
+        If True is specified, all the nonsilence is kept, if False none is kept.
+        default: 100ms
+
+    seek_step - step size for interating over the segment in ms
+    """
+
+    # from the itertools documentation
+    def pairwise(iterable):
+        "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+    if isinstance(keep_voice, bool):
+        keep_voice = len(audio_segment) if keep_voice else 0
+
+    output_ranges = [
+        [ start - keep_voice, end + keep_voice ]
+        for (start,end)
+            in detect_silent(audio_segment, min_silence_len, silence_thresh, seek_step)
+    ]
+
+    for range_i, range_ii in pairwise(output_ranges):
+        last_end = range_i[1]
+        next_start = range_ii[0]
+        if next_start < last_end:
+            range_i[1] = (last_end+next_start)//2
+            range_ii[0] = range_i[1]
+
+    return [
+        audio_segment[ max(start,0) : min(end,len(audio_segment)) ]
+        for start,end in output_ranges
+    ]
 
 
 def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
