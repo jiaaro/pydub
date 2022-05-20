960500d45355cad5c12d4d2f13a912c38f82b8e0
Vlad <loj.jol.st@yandex.ru>
2022-05-20 19:39:42 +0500

    add 1.txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Manipulate audio with an simple and easy high level interface.
 
 See the README file for details, usage info, and a list of gotchas.
 """
-
+//11111
 from setuptools import setup
 
 setup(
