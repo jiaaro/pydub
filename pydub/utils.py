cd376137377fa78ca0eae16c89d40249663e707a
Gonzalo <godiard@gmail.com>
2025-01-13 11:41:11 -0300

    fix import of local pyaudioop if audioop is missing (python > 3.12)

--- pydub/utils.py
+++ pydub/utils.py
@@ -14,7 +14,7 @@ from functools import wraps
 try:
     import audioop
 except ImportError:
-    import pyaudioop as audioop
+    from . import pyaudioop as audioop
 
 if sys.version_info >= (3, 0):
     basestring = str
